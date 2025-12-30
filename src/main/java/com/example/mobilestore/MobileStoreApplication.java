8cd7911bdfca4fc2d89576b745edfa5da881adfa
chaitanya461 <saipureddysai17@gmail.com>
2025-12-30 14:47:09 +0530

    Update MobileStoreApplication.java

--- src/main/java/com/example/mobilestore/MobileStoreApplication.java
+++ src/main/java/com/example/mobilestore/MobileStoreApplication.java
@@ -1,13 +1,18 @@
-package com.example.mobilestore;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MobileStoreApplication {
+public class MobileStoreApplication extends SpringBootServletInitializer {
+
+    // IMPORTANT: Add this method to allow deployment to external Tomcat
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MobileStoreApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MobileStoreApplication.class, args);
     }
 }
-
